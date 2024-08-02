2a45edd7282557d1ec126b9e48b8f81cbee00bef
subinsong01 <haely97@naver.com>
2024-08-02 15:19:23 +0900

    fix: css

--- main.js
+++ main.js
@@ -9,25 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
     let lengthItems = items.length;
 
     next.onclick = function() {
-        if (active < lengthItems - 1) {
+        if (active < lengthItems - 1) { // 다음 이미지가 있다면
             active++;
-            reloadSlider();
+        } else {
+            active = 0; // 마지막 이미지 후 첫 번째 이미지로 돌아감
         }
+        reloadSlider();
     };
 
     prev.onclick = function() {
         if (active > 0) {
             active--;
-            reloadSlider();
+        } else {
+            active = lengthItems - 1; // 첫 번째 이미지에서 이전 버튼을 누르면 마지막 이미지로 이동
         }
+        reloadSlider();
     };
 
     function reloadSlider() {
         let checkLeft = items[active].offsetLeft;
-        list.style.transform = `translateX(-${checkLeft}px)`;
+        list.style.transform = `translateX(-${checkLeft}px)`; // 슬라이드 위치 조정
 
-        dots.forEach(dot => dot.classList.remove('active'));
-        dots[active].classList.add('active');
+        dots.forEach(dot => dot.classList.remove('active')); // 모든 점의 활성화 상태 제거
+        dots[active].classList.add('active'); // 현재 활성화된 슬라이드에 대한 점 활성화
     }
 
     reloadSlider(); // 초기 슬라이더 로드
